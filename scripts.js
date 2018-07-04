cecc6fd8e8bb7089a6177e1414c184d3b56bd174
walker80314 <40841001+walker80314@users.noreply.github.com>
2018-07-04 18:44:01 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,7 +16,8 @@ function setupListeners() {
     }
   );
 
-    $('#intro-ct').hover(
+  //
+  $('#intro-ct').hover(
     function() {
       $('#intro-ct').stop(true, true);
       $('#intro-ct').animate({bottom: '5px'}, 150, 'easeInCubic');
@@ -24,6 +25,38 @@ function setupListeners() {
     function() {
       $('#intro-ct').stop(true, true);
       $('#intro-ct').animate({bottom: '0px'}, 150, 'easeInCubic');
+    }
+  );
+
+  //
+  $('#sidebar-about').hover(
+    function() {
+      $('#sidebar-work, #sidebar-illus').stop(true, true);
+      $('#sidebar-work, #sidebar-illus').animate({opacity: '0.3'}, 50, 'easeInCubic');
+    }, 
+    function() {
+      $('#sidebar-work, #sidebar-illus').stop(true, true);
+      $('#sidebar-work, #sidebar-illus').animate({opacity: '1.0'}, 50, 'easeInCubic');
+    }
+  );
+  $('#sidebar-work').hover(
+    function() {
+      $('#sidebar-about, #sidebar-illus').stop(true, true);
+      $('#sidebar-about, #sidebar-illus').animate({opacity: '0.3'}, 50, 'easeInCubic');
+    }, 
+    function() {
+      $('#sidebar-about, #sidebar-illus').stop(true, true);
+      $('#sidebar-about, #sidebar-illus').animate({opacity: '1.0'}, 50, 'easeInCubic');
+    }
+  );
+  $('#sidebar-illus').hover(
+    function() {
+      $('#sidebar-work, #sidebar-about').stop(true, true);
+      $('#sidebar-work, #sidebar-about').animate({opacity: '0.3'}, 50, 'easeInCubic');
+    }, 
+    function() {
+      $('#sidebar-work, #sidebar-about').stop(true, true);
+      $('#sidebar-work, #sidebar-about').animate({opacity: '1.0'}, 50, 'easeInCubic');
     }
   );
 }
